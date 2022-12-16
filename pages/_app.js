753e0f7286e66fda5d716f3a5abc6c87cfd1512f
SN4KEBLACK <mretamozo@proton.me>
2022-12-16 16:16:37 +0100

    fix: _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,8 @@
-import '../styles/globals.css'
-import 'react-toastify/dist/ReactToastify.css'
-import Head from 'next/head'
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta charSet='utf-8' />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
