4d02fa68e7aa7ebd25acabf82e75900e9a106184
SN4KEBLACK <mretamozo@proton.me>
2022-12-16 16:41:50 +0100

    update: add icon and add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
-import "../styles/globals.css";
-import "react-toastify/dist/ReactToastify.css";
+import '../styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Device Manager</title>
+        <meta 
+          property='og:title'
+          content='Device Manager'
+          key='title'
+        />
+        <meta
+          name='viewport'
+          content='initial-scale=1.0, width=device-width'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
