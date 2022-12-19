fac781cd95078ed77d14afaad9f7d5882dcf1b8b
SN4KEBLACK <mretamozo@proton.me>
2022-12-19 20:49:22 +0100

    test: testing sidebar

--- helpers/height.js
+++ helpers/height.js
@@ -6,7 +6,8 @@ import UAParser from 'ua-parser-js';
 export function height() {
   const router = useRouter();
   const parser = new UAParser(userAgent);
-  const screenHeight = parser.getDevice().model === 'Android' ? 'pb-60' : '';
+  // si es chrome mobile y brave mobile que devuelvan pb-60 si no nada
+  const screenHeight = parser.getEngine().name === 'Blink' && parser.getDevice().type === 'mobile' ? 'pb-60' : '';
 
   return screenHeight;
 }
